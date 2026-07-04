a17aee666f4834111239f8bf9c9f6e0a982f5e81
bekhruzbb <behruz10mominjonom@gmail.com>
2026-07-04 18:46:28 +0500

    Fix: production static root configuration for vercel

--- project/settings.py
+++ project/settings.py
@@ -142,7 +142,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
