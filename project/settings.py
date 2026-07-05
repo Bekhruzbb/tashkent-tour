3204969b5beee1670f94b5ef2de30542dedbd27b
bekhruzbb <behruz10mominjonom@gmail.com>
2026-07-05 11:00:20 +0500

    change settings.py

--- project/settings.py
+++ project/settings.py
@@ -108,6 +108,15 @@ DATABASES = {
         },
     }
 }
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
