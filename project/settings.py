b314f4d6356a082b004588cb4c75e117241e3683
bekhruzbb <behruz10mominjonom@gmail.com>
2026-07-05 16:55:12 +0500

    change settings.py

--- project/settings.py
+++ project/settings.py
@@ -164,7 +164,6 @@ STATICFILES_DIRS = [
 ]
 
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = "media"
@@ -173,7 +172,14 @@ AWS_S3_REGION_NAME = "us-east-1"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = "public-read"
 
-
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 from datetime import timedelta
 
